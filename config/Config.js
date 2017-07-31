73405f993c773c637475b77a571855858e99405b
Njagi Mwaniki <njagi@urbanslug.com>
2017-07-31 13:08:36 +0300

    Add a dedicated config module.
    Added a config/Config.js to hold all cofigs and set defaults instead of
    replicating env vars.
    
    Fixed linting errors.
    
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

---
+++ config/Config.js
@@ -0,0 +1,23 @@
+const env = process.env;
+
+module.exports = {
+  // core
+  environment: env.NODE_ENV || 'dev', // dev, prod, test
+  PORT: env.PORT || 3000, // env to run karma on
+
+  // facebook
+  facebookPageAccessToken: env.FACEBOOK_PAGE_ACCESS_TOKEN,
+  facebookAppSecret: env.FACEBOOK_APP_SECRET,
+  facebookApiVersion: env.FACEBOOK_API_VERSION || 'v2.6',
+  facebookVerifyToken: env.FACEBOOK_VERIFY_TOKEN || 'karma', // webhook verification token
+
+  // external data stores
+  onadataApiToken: env.ONADATA_API_TOKEN,
+  rapidproApiToken: env.RAPIDPRO_API_TOKEN,
+
+  // logging and error reporting
+  sentryDSN: env.SENTRY_DSN,
+  karmaAccessLogFile: env.KARMA_ACCESS_LOG_FILE || 'karma_http.log',
+  karmaErrorFile: env.KARMA_ERROR_FILE || 'karma_errors.log',
+  debugTranslations: env.DEBUG_TRANSLATIONS === 'true' || false,
+};
