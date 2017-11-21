6e32593d21346d59cfc728628f4b4509c282b40f
Njagi Mwaniki <njagi@urbanslug.com>
2017-11-21 13:48:47 +0300

    Add conversation interrupt handler
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const setup = require('./lib/setup.js');
 const {
+  helpConversation,
   prepareConversation,
   createUserAndStartConversation,
 } = require('./lib/user.js');
@@ -11,32 +12,12 @@ const {
   translate: t,
   localeUtils,
 } = require('borq');
-const {
-  generateButtonTemplate,
-} = facebookUtils;
+const {utterances} = facebookUtils;
+
 const {controller} = facebook;
 const karma = facebook.controller.spawn({});
 let lang = config.defaultLanguage;
 setup(karma);
-
-/**
-* A helper function to DRY creating yes and no button templates
-* @param {string} text The question text
-* @param {string} payloadRaary the payloads for yes and no in an array
-* @param {string} lang ISO6392 language code string
-* @return {object} returns a JS object that is sent to FB to create a button
-*/
-function generateYesNoButtonTemplate(text, [yesPayload, noPayload], lang) {
-  return generateButtonTemplate(text,
-                                null,
-                                [{
-                                  title: t(`${lang}:yes`),
-                                  payload: yesPayload,
-                                }, {
-                                  title: t(`${lang}:no`),
-                                  payload: noPayload,
-                                }]);
-}
 
 
 controller.on('facebook_postback', (bot, message) => {
@@ -58,55 +39,17 @@ controller.on('facebook_referral', (bot, message) => {
   createUserAndStartConversation(message, bot);
 });
 
-const utterances = {
-  yes: new RegExp(/^(yes*|yea|yup|yep|ya|sure|ok|y|yeah|yah|ya|sim)/i),
-  no: new RegExp(/^(no*|nah|nope|n|tidak|não)/i),
-  greetings: ['help',
-              '👋',
-              'hello',
-              '(^restart(?!_))',
-              'mulai',
-              'halo',
-              'hi',
-              'oi',
-              'hai',
-              'membantu',
-              'socorro',
-              'olá'],
-};
-
 controller.hears(
   utterances.greetings,
   'message_received',
   (bot, message) => {
-    bot.startConversation(message, (err, convo) => {
-      convo.addQuestion(
-        generateYesNoButtonTemplate(t(`${lang}:utils.helpMessage`),
-                                    ['yesPayload', 'noPayload'],
-                                    lang),
-        [
-          {
-            pattern: utterances.yes,
-            callback: (res, convo) => {
-              convo.stop();
-              prepareConversation(bot, message);
-            },
-          }, {
-            pattern: utterances.no,
-            callback: (res, convo) => {
-              convo.stop();
-              bot.reply(message, t(`${lang}:utils.quitMessage`));
-            },
-          }, {
-            default: true,
-            callback: (res, conv) => {
-              conv.say(t(`${lang}:utils.pressYN`));
-              conv.repeat();
-              conv.next();
-            },
-          },
-        ]);
-    });
+    const {user} = message;
+    services.getUser({urn: `facebook:${user}`})
+      .then(
+      ({results: [{language}]}) => {
+        helpConversation(bot, message, localeUtils.lookupISO6391(language));
+      })
+      .catch((err) => helpConversation(bot, message, lang));
   }
 );
 
