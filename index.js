2175c035a252e43bdb5dc6626c305cb6f388f6d7
Njagi Mwaniki <njagi@urbanslug.com>
2018-05-18 15:19:36 +0300

    Finish on quit conversation
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- index.js
+++ index.js
@@ -23,20 +23,24 @@ let lang = config.defaultLanguage;
 setup(karma);
 
 controller.on('facebook_postback', async (bot, message) => {
-  const contact = await getContact(message.user);
-  if (contact) {
-    const {payload, user} = message;
-    if (['get_started'].includes(payload)) {
-      prepareConversation(bot, message, contact, 'get started');
-    } else if (['switch_pt', 'switch_en', 'switch_id'].includes(payload)) {
-      prepareConversation(bot, message, contact, 'change language');
-    } else if (['restart_survey'].includes(payload)) {
-      prepareConversation(bot, message, contact, 'restart');
-    } else if (['quit', 'opt_out'].includes(payload)) {
-      quitConversation(bot, message, contact);
+  try {
+    const contact = await getContact(message.user);
+    if (contact) {
+      const {payload, user} = message;
+      if (['get_started'].includes(payload)) {
+        prepareConversation(bot, message, contact, 'get started');
+      } else if (['switch_pt', 'switch_en', 'switch_id'].includes(payload)) {
+        prepareConversation(bot, message, contact, 'change language');
+      } else if (['restart_survey'].includes(payload)) {
+        prepareConversation(bot, message, contact, 'restart');
+      } else if (['quit', 'opt_out'].includes(payload)) {
+        quitConversation(bot, message, contact);
+      }
+    } else {
+      consentConversation(bot, message);
     }
-  } else {
-    consentConversation(bot, message);
+  } catch (e) {
+    // TODO: log this
   }
 });
 
@@ -59,7 +63,7 @@ controller.hears(
   'message_received',
   async (bot, message) => {
     try {
-      const {body: {results: [{language}]}} = await services.getUser({urn: `facebook:${message.user}`});
+      const {language} = await getContact(message.user);
       if (language) {
         bot.reply(message, t(`${localeUtils.lookupISO6391(language)}:utils.idkw`));
       } else {
